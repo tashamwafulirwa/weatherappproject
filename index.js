e688a04b485f8b49cfdd34e8ebcb7deae8d5f14c
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-18 17:01:11 +0100

    created javascript file
    copied my javascript from codesandbox to vs code

---
+++ index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let apiKey = "532204b610112a0c24a32e14ce6c2371";
+  let apiUrL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrL).then(displayWeatherCondition);
+}
+
+let day = days[now.getDay()];
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
+
+h2.innerHTML = `${day} ${hours}:${minutes}`;
