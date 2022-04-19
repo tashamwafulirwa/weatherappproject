d5fcca05ee4421dba33413d381b8323aa7ecce75
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-19 22:12:07 +0100

    removed console log

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature (response) {
-console.log(response.data);
 let temperatureElement = document.querySelector("temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
