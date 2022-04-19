d5bc1c14e84deb11de0b6e68840c6e111bec0b40
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-19 21:51:20 +0100

    showing the current temperature js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ console.log(response.data);
 let temperatureElement = document.querySelector("temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round
 (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "532204b610112a0c24a32e14ce6c237";
