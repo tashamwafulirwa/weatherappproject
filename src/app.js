bca6629ce4eb8b1bc8045e54b88e6e386ca588ac
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-19 19:37:36 +0100

    redid javascript
    my javascript was not working so i deleted the folder and started again.

--- src/app.js
+++ src/app.js
@@ -1,42 +1,15 @@
-import "./styles.css";
-
-let now = new Date();
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let dateElement = document.querySelector("#date");
-dateElement.innerHTML = `${day} ${hours}:${minutes}`;
-
-let h2 = document.querySelector("h2");
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+function displayTemperature (response) {
+console.log(response.data);
+let temperatureElement = document.querySelector("temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+temperatureElement.innerHTML = Math.round
+(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  let apiKey = "532204b610112a0c24a32e14ce6c2371";
-  let apiUrL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrL).then(displayWeatherCondition);
-}
+let apiKey = "532204b610112a0c24a32e14ce6c237";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let searchForm = document.querySelector("#search-form");
-
-searchForm.addEventListener("submit", search);
-
-h2.innerHTML = `${day} ${hours}:${minutes}`;
-
+axios.get(apiUrl).then(displayTemperature);
