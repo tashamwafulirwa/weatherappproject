fa3caaac38ef41452a155498e3c74a61abf0f807
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-20 15:05:54 +0100

    update app.js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
-iconElement.setAttribute( "src", 'http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png');
+iconElement.setAttribute( "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
