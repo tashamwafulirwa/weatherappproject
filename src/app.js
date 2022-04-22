cb17cb07ef8c79d5290c93b276f0fa9fc5d72dc9
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-22 23:12:23 +0100

    updated date

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function formatDay(timestamp) {
-let date = new date (timestamp * 1000);
+let date = new Date(timestamp * 1000);
 let day = date.getDay();
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
